29d7acedf4815db2e98898016b7c8a46a19f2eb2
wusus0202 <webberwusus@gmail.com>
2026-01-05 09:44:48 +0800

    Add initial implementation of main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+let currentSource = 'A';
+const sourceConfig = { ... };
+function switchPage(...) { ... }
+function fetchPlantData() { ... }
+function fetchData() { ... }
+function updateClock() { ... }
